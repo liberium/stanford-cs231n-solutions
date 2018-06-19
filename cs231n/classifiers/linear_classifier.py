741950f4eb0efcbec5de64e5fcbbbac8fe58cb51
Sergey Filkin <sergey.filkin@gmail.com>
2018-06-19 22:07:10 +0300

    implement LinearClassifier.predict()

--- cs231n/classifiers/linear_classifier.py
+++ cs231n/classifiers/linear_classifier.py
@@ -92,7 +92,7 @@ class LinearClassifier(object):
         # TODO:                                                                   #
         # Implement this method. Store the predicted labels in y_pred.            #
         ###########################################################################
-        pass
+        y_pred = np.argmax(X @ self.W, axis=1)
         ###########################################################################
         #                           END OF YOUR CODE                              #
         ###########################################################################
