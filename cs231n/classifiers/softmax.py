83311b7a903fe87310da1de4b2b2d199fc117386
Sergey Filkin <sergey.filkin@gmail.com>
2018-06-23 23:57:35 +0300

    implement softmax loss

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -23,6 +23,7 @@ def softmax_loss_naive(W, X, y, reg):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
+    num_samples = X.shape[0]
 
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using explicit loops.     #
@@ -30,7 +31,8 @@ def softmax_loss_naive(W, X, y, reg):
     # here, it is easy to run into numeric instability. Don't forget the        #
     # regularization!                                                           #
     #############################################################################
-    pass
+    for i in num_samples:
+        pass
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
