fix softmax gradient

cs231n/classifiers/softmax.py
@@ -35,20 +35,22 @@ def softmax_loss_naive(W, X, y, reg):
         scores = (X[i] @ W).T  # shape (C, 1)
         max_score = np.max(scores)
         normalised_correct_class_score = \
-            np.exp(scores[y[i]] - max_score) / np.sum(np.exp(scores - max_score))
+            np.exp(scores[y[i]] - max_score) \
+            / np.sum(np.exp(scores - max_score))
         loss += - np.log(normalised_correct_class_score)
+        dW_i = np.zeros_like(dW)
         exp_sum = .0
         for j in range(num_classes):
             if j == y[i]:
                 exp_sum += 1
                 continue
-            score_diff = X[i] @ (W[:, j] - W[:, y[i]])
+            score_diff = X[i] @ (W[:, j] - W[:, y[i]])  # TODO: optimise
             exp_score_diff = np.exp(score_diff)
-            dW[:, j] = exp_score_diff * X[i]
+            dW_i[:, j] = exp_score_diff * X[i]
             exp_sum += exp_score_diff
-        dW[range(num_classes), y] = - X[i] * (exp_sum - 1)
-        dW /= exp_sum
-
+        dW_i[:, y[i]] = - X[i] * (exp_sum - 1)
+        dW_i /= exp_sum
+        dW += dW_i
     loss /= num_samples
     dW /= num_samples
     loss += reg * np.sum(W ** 2)